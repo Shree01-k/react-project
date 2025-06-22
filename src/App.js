9f4964a830bf5fd16497bd433bde40b8dde88b37
Shrinath Kharad <kharadshrinath84@gmail.com>
2025-06-22 13:32:37 +0530

    sdfdf

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import './App.css';
-import Narbar from './components/Narbar';
 import About from './components/About';
 import Contact from './components/Contact';
 import Services from './components/Services';
 import Home from './components/Home';
+import Navbar from "./components/Narbar"
 
 function App() {
   return (
-    <>
-      <Router>
-        <Narbar />
-        <Routes>
-          <Route path='/about' element={<About name={"Shrinath"} />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/services' element={<Services />} />
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/about' element={<About name={"Shrinath"} />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Home() {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [sum, setSum] = useState(null);
+
+  const handleCalculate = () => {
+    setSum(Number(num1) + Number(num2));
+  };
+
   return (
     <div>
-        {/* this is abou page */}
-       I am in home page
+      <h2>Home Page: Add Two Numbers</h2>
+      <input type="number" value={num1} onChange={(e) => setNum1(e.target.value)} />
+      <input type="number" value={num2} onChange={(e) => setNum2(e.target.value)} />
+      <button onClick={handleCalculate}>Calculate Sum</button>
+      {sum !== null && <p>Sum: {sum}</p>}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
